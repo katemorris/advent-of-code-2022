954c74f69c540ceaf20ac98ebb56c17c370f797b
Kate Tester <katemorris@gmail.com>
2022-12-06 10:27:29 -0700

    Day 6

---
+++ 6-message-search.py
@@ -0,0 +1,23 @@
+with open("inputs/6.txt", "r") as file:
+    for line in file.readlines():
+        letter_list = []
+        for index, char in enumerate(line):
+            # print(f"character {index+1}: {char} and list: {letter_list}")
+            letter_list.append(char)
+            # print(letter_list)
+            list_len = len(letter_list)
+            if list_len > 14 and list_len > 1:
+                letter_list.pop(0)
+                # print(f"after removal list {letter_list}")
+            list_len = len(letter_list)
+            # print(list_len, letter_list)
+            if list_len == 14 and len(set(letter_list)) < list_len:
+                # print(f"skipping, duplicates {set(letter_list)}")
+                continue
+            elif list_len == 14 and len(set(letter_list)) == list_len:
+                print(f"success! {letter_list}")
+                print(index + 1)
+                break
+            else:
+                # print("do what?")
+                continue
