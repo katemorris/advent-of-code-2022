156ebf90f6c00cf50c393f52a993a70796308c44
Kate Tester <katemorris@gmail.com>
2022-12-21 19:11:19 -0700

    Finish day 11

--- 11-thou-monkey-biz.py
+++ 11-thou-monkey-biz.py
@@ -1,11 +1,14 @@
 import code
 import math
+import sys
+sys.setrecursionlimit(20000)
 
-with open("inputs/11-short.txt", "r") as file:
+with open("inputs/11.txt", "r") as file:
     monkeys = file.read().split("\n\n")
 
 
 monkey_list = {}
+divisors = 1
 for mankey in monkeys:
     attributes = mankey.split("\n")
     for line in attributes:
@@ -23,6 +26,7 @@ for mankey in monkeys:
             # print(operation, value)
         if "Test:" in line:
             test_divisor = int(line.replace("Test: divisible by ", ""))
+            divisors *= test_divisor
             # print(test_divisor)
         if "If true" in line:
             true_monkey = int(line.replace("If true: throw to monkey ", ""))
@@ -62,6 +66,7 @@ def do_rounds(monkey_list, rounds):
                 elif att["operation"] == "*":
                     item *= value
                     # print(f"item {item} post multiply")
+                item = item % divisors
                 # test divisor
                 if item % att["test_divisor"] == 0:
                     monkey_list[att["true_monkey"]]["items"].append(item)
@@ -75,9 +80,11 @@ def do_rounds(monkey_list, rounds):
 
 
 print(monkey_list)
-rounds = 1000
+rounds = 10000
 final = do_rounds(monkey_list, rounds)
 print(final)
 inspections = [atts["inspections"] for atts in final.values()]
 sorted_insp = sorted(inspections)
 print(sorted_insp[-1] * sorted_insp[-2])
+
+#too low 2713310158
